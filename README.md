f25ebc3d05e2658ec41e98b66d62cbfce7d6b971
Jason <jasoncutrona@gmail.com>
2016-06-13 17:54:04 +1000

    third revision

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 Example of simple image carousel as found on [The Odin Project](http://www.theodinproject.com)   
     
 #Update log
+**13/6/16** Added thumbnail functionality. Added hover css effects for navigation buttons.
 **13/6/16** Implemented navigation buttons. Removed redundancies in script. Updated CSS. Updated readme
 **12/6/16** Uploaded first working version of project. Functionality includes sliding left and right through a series of five images.
 **10/6/16** Updated readme
@@ -10,4 +11,4 @@ Example of simple image carousel as found on [The Odin Project](http://www.theod
 #To Do
 - Implement wrapping functionality (after last image scroll to first and vice versa)
 - Make slider look more visually appealing to look at
-- Optional: Show thumbnails when hovering over navigation items
+
--- scripts/slider.js
+++ scripts/slider.js
@@ -10,6 +10,7 @@ function nextSlide(slideNumber) {
             left: SCROLL_RIGHT},SCROLL_SPEED);
             slideNumber += 1;
         }
+    activeSlide(slideNumber);
     return slideNumber;
 };
 
@@ -19,6 +20,7 @@ function prevSlide(slideNumber) {
             left: SCROLL_LEFT},SCROLL_SPEED);
             slideNumber -=1;
         }
+    activeSlide(slideNumber);
     return slideNumber;
 }
 
@@ -27,31 +29,54 @@ function goToSlide($buttonNumber, slideNumber) {
     var position = (640*slideNumber*-1)+640;
     $('.frame_picture').animate({
         left: position},SCROLL_SPEED);
+    activeSlide(slideNumber);
     return slideNumber;    
+};
+
+function showThumb(e,$buttonNumber){
+    var mouseX = e.pageX + "px";
+    var mouseY = e.pageY + "px";
+    $('#thumbnail').css("left",mouseX);
+    $('#thumbnail').css("top",mouseY);
+    $('#thumbnail').html("<img src='images/thumbs/zelda"+$buttonNumber+"t.jpg'>");
+    $('#thumbnail').css("visibility","visible");
+
+};
+
+function activeSlide(slideNumber) {
+    $('.frame_nav_button').css("border","none");
+    $('.frame_nav_button:eq('+(slideNumber-1)+')').css("border","1px solid orange");
 }
 
 $('document').ready(function () {
     
     var slideNumber = 1;
-    
+    var $buttonNumber;
     //Slide the pictures left and write upon appropriate button click
     $('#frame_slide_left').click(function () {
         slideNumber = prevSlide(slideNumber);
     });
     $('#frame_slide_right').click(function () {
         slideNumber = nextSlide(slideNumber);
-        console.log($('.frame_picture').css("left"));
     });
     $('.frame_nav_button').click(function () {
-        var $buttonNumber = $('.frame_nav_button').index(this)+1;
+        $buttonNumber = $('.frame_nav_button').index(this)+1;
         slideNumber = goToSlide($buttonNumber,slideNumber);
+    });
+    $('.frame_nav_button').hover(function (e) {
+        $buttonNumber = $('.frame_nav_button').index(this)+1;
+        showThumb(e,$buttonNumber);
+    })
+    $('.frame_nav_button').mouseleave(function() {
+        $('#thumbnail').css("visibility","hidden");
     })
 
 
     
     //Automatically scroll through
     var timer = setInterval(function() {
-        slideNumber = nextSlide(slideNumber)},50000)
+        slideNumber = nextSlide(slideNumber);
+        },5000)
 });
     
     
