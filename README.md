d449d6c6f27cbcf01583a87e178ebc2e098d21ff
Jason <jasoncutrona@gmail.com>
2016-06-14 21:51:37 +1000

    fourth revision

--- README.md
+++ README.md
@@ -3,10 +3,10 @@
 Example of simple image carousel as found on [The Odin Project](http://www.theodinproject.com)   
     
 #Update log
-**13/6/16** Added thumbnail functionality. Added hover css effects for navigation buttons.
-**13/6/16** Implemented navigation buttons. Removed redundancies in script. Updated CSS. Updated readme
-**12/6/16** Uploaded first working version of project. Functionality includes sliding left and right through a series of five images.
-**10/6/16** Updated readme
+**13/6/16** Added thumbnail functionality. Added hover css effects for navigation buttons.  
+**13/6/16** Implemented navigation buttons. Removed redundancies in script. Updated CSS. Updated readme.  
+**12/6/16** Uploaded first working version of project. Functionality includes sliding left and right   through a series of five images.  
+**10/6/16** Updated readme.
 
 #To Do
 - Implement wrapping functionality (after last image scroll to first and vice versa)
--- scripts/slider.js
+++ scripts/slider.js
@@ -2,37 +2,54 @@
 const SCROLL_SPEED = 500;
 const SCROLL_LEFT = "+=640";
 const SCROLL_RIGHT = "-=640";
+const REEL_WIDTH = "+=3200px";
 
 //Functions for sliding
 function nextSlide(slideNumber) {
-    if (slideNumber < 5){
+    //Wrapping
+    if (slideNumber >= 6) {
+        $('.frame_picture').css('left','+=3200');
+        slideNumber = 1; 
+        activeSlide(1);
+    };
+    
+    if (slideNumber < 6){
         $('.frame_picture').animate({
-            left: SCROLL_RIGHT},SCROLL_SPEED);
-            slideNumber += 1;
-        }
-    activeSlide(slideNumber);
+        left: SCROLL_RIGHT},SCROLL_SPEED);
+        slideNumber += 1;
+        activeSlide(slideNumber);
+    };
+    console.log(slideNumber);
     return slideNumber;
 };
 
 function prevSlide(slideNumber) {
-    if (slideNumber > 1){
+    //Wrapping
+    if (slideNumber === 0) {
+        $('.frame_picture').css('left','-=3200')
+        slideNumber = 5; 
+        activeSlide(5);    
+    };
+    
+    if (slideNumber > -1){
+        
         $('.frame_picture').animate({
             left: SCROLL_LEFT},SCROLL_SPEED);
             slideNumber -=1;
-        }
+        };
     activeSlide(slideNumber);
     return slideNumber;
 }
 
 function goToSlide($buttonNumber, slideNumber) {
     slideNumber = $buttonNumber;
-    var position = (640*slideNumber*-1)+640;
+    var position = (640*slideNumber*-1);
     $('.frame_picture').animate({
         left: position},SCROLL_SPEED);
     activeSlide(slideNumber);
     return slideNumber;    
 };
-
+//Thumbnail functionality
 function showThumb(e,$buttonNumber){
     var mouseX = e.pageX + "px";
     var mouseY = e.pageY + "px";
@@ -40,13 +57,28 @@ function showThumb(e,$buttonNumber){
     $('#thumbnail').css("top",mouseY);
     $('#thumbnail').html("<img src='images/thumbs/zelda"+$buttonNumber+"t.jpg'>");
     $('#thumbnail').css("visibility","visible");
-
 };
 
 function activeSlide(slideNumber) {
+    if (slideNumber === 6){
+        slideNumber = 1;
+    };
     $('.frame_nav_button').css("border","none");
     $('.frame_nav_button:eq('+(slideNumber-1)+')').css("border","1px solid orange");
-}
+};
+
+function createEndFrames (slideNumber) {
+    
+    var $finalFrame = "<div class='frame_picture'>"+$('.frame_picture:eq(0)').html()+"</div>";
+    var $firstFrame =  "<div class='frame_picture'>"+$('.frame_picture:eq(4)').html()+"</div>";
+    
+    $('#frame_photo_reel').append($finalFrame);
+    $('#frame_photo_reel').prepend($firstFrame);
+    $('.frame_picture').css('left','-=640');
+    return slideNumber -=1;    
+};
+
+
 
 $('document').ready(function () {
     
@@ -66,17 +98,18 @@ $('document').ready(function () {
     $('.frame_nav_button').hover(function (e) {
         $buttonNumber = $('.frame_nav_button').index(this)+1;
         showThumb(e,$buttonNumber);
-    })
+    });
     $('.frame_nav_button').mouseleave(function() {
         $('#thumbnail').css("visibility","hidden");
-    })
-
-
+    });
+    //Initialize
+    createEndFrames(slideNumber);
+    activeSlide(slideNumber);
     
     //Automatically scroll through
-    var timer = setInterval(function() {
+  k var timer = setInterval(function() {
         slideNumber = nextSlide(slideNumber);
-        },5000)
+    },5000);
 });
     
     
